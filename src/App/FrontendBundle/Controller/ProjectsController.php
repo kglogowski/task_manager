287f268d1072921aaadd8a3dbd314c1c815b6be4
Qboss <kubos444@interia.pl>
2014-06-04 13:06:38 +0200

    opis2

--- src/App/FrontendBundle/Controller/ProjectsController.php
+++ src/App/FrontendBundle/Controller/ProjectsController.php
@@ -12,38 +12,12 @@ class ProjectsController extends TmController {
     public function indexAction() {
         $m = $this->getDoctrine()->getManager();
         $uzytkownik = $this->getUser();
-
-        $collMyProjekt = $m->createQuery("
-            SELECT p.label, p.status as status_projektu, p.name, up.rola as rola_uzytkownika, p.termin as termin
-                FROM DataDatabaseBundle:Projekt p
-                LEFT JOIN DataDatabaseBundle:UzytkownikProjekt up WITH p.id = up.projekt
-                WHERE up.uzytkownik = :uzytkownik_id
-                AND p.status != :status_zamkniety
-                AND p.skasowane is null
-        ")->setParameter(':uzytkownik_id', $uzytkownik->getId())->setParameter(':status_zamkniety', Projekt::STATUS_ZAMKNIETY)
-                ->getResult();
-        $projektSubQuery = $m->createQuery("                    
-            SELECT pp.id
-                        FROM DataDatabaseBundle:UzytkownikProjekt up
-                        JOIN DataDatabaseBundle:Projekt pp WITH pp.id = up.projekt
-                        WHERE up.uzytkownik = :uzytkownik_id
-        ")
-                ->setParameter(':uzytkownik_id', $uzytkownik->getId())
-                ->getResult();
-        $projekt = $m->createQuery("
-            SELECT p
-                FROM DataDatabaseBundle:Projekt p
-                WHERE p.id NOT IN (
-                    :array
-                )
-                AND p.status != :status_zamkniety
-        ")
-                ->setParameter(':status_zamkniety', Projekt::STATUS_ZAMKNIETY)
-                ->setParameter(':array', $projektSubQuery)
-                ->getResult();
+        $projektRepo = $m->getRepository('DataDatabaseBundle:Projekt');
+        $collMyProjekt = $projektRepo->getProjectByUser($uzytkownik);
+        $restProject = $projektRepo->getRestOfProject($uzytkownik);
         return $this->render('AppFrontendBundle:Projects:index.html.twig', array(
                     'myProjects' => $collMyProjekt,
-                    'restProjects' => $projekt,
+                    'restProjects' => $restProject,
                     'UzytkownikProjekt' => new UzytkownikProjekt(),
                     'Projekt' => new Projekt()
         ));
@@ -52,46 +26,12 @@ class ProjectsController extends TmController {
     public function zakonczoneAction() {
         $m = $this->getDoctrine()->getManager();
         $uzytkownik = $this->getUser();
-
-        $collMyProjekt = $m->createQuery("
-            SELECT p.label, p.status as status_projektu, p.name, up.rola as rola_uzytkownika, p.termin as termin
-                FROM DataDatabaseBundle:Projekt p
-                LEFT JOIN DataDatabaseBundle:UzytkownikProjekt up WITH p.id = up.projekt
-                WHERE up.uzytkownik = :uzytkownik_id
-                AND p.status = :status_zamkniety
-                AND p.skasowane is null
-        ")->setParameter(':uzytkownik_id', $uzytkownik->getId())->setParameter(':status_zamkniety', Projekt::STATUS_ZAMKNIETY)
-                ->getResult();
-        
-                $projektSubQuery = $m->createQuery("                    
-            SELECT pp.id
-                        FROM DataDatabaseBundle:UzytkownikProjekt up
-                        JOIN DataDatabaseBundle:Projekt pp WITH pp.id = up.projekt
-                        WHERE up.uzytkownik = :uzytkownik_id
-        ")
-                ->setParameter(':uzytkownik_id', $uzytkownik->getId())
-                ->getResult();
-        $projekt = $m->createQuery("
-            SELECT p
-                FROM DataDatabaseBundle:Projekt p
-                WHERE p.id NOT IN (
-                    :array
-                )
-                AND p.status = :status_zamkniety
-        ")
-                ->setParameter(':status_zamkniety', Projekt::STATUS_ZAMKNIETY)
-                ->setParameter(':array', $projektSubQuery)
-                ->getResult();
-        return $this->render('AppFrontendBundle:Projects:index.html.twig', array(
-                    'myProjects' => $collMyProjekt,
-                    'restProjects' => $projekt,
-                    'UzytkownikProjekt' => new UzytkownikProjekt(),
-                    'Projekt' => new Projekt()
-        ));
-
+        $projektRepo = $m->getRepository('DataDatabaseBundle:Projekt');
+        $collMyProjekt = $projektRepo->getProjectByUserZakonczone($uzytkownik);
+        $restProject = $projektRepo->getRestOfProjectZakonczone($uzytkownik);
         return $this->render('AppFrontendBundle:Projects:zakonczone.html.twig', array(
                     'myProjects' => $collMyProjekt,
-            'restProjects' => $projekt,
+                    'restProjects' => $restProject,
                     'UzytkownikProjekt' => new UzytkownikProjekt(),
                     'Projekt' => new Projekt()
         ));
@@ -100,43 +40,13 @@ class ProjectsController extends TmController {
     public function skasowaneAction() {
         $m = $this->getDoctrine()->getManager();
         $uzytkownik = $this->getUser();
+        $projektRepo = $m->getRepository('DataDatabaseBundle:Projekt');
+        $collMyProjekt = $projektRepo->getProjectByUserZakonczone($uzytkownik);
+        $restProject = $projektRepo->getRestOfProjectZakonczone($uzytkownik);
 
-        $collMyProjekt = $m->createQuery("
-            SELECT p.label, p.status as status_projektu, p.name, up.rola as rola_uzytkownika, p.termin as termin
-                FROM DataDatabaseBundle:Projekt p
-                LEFT JOIN DataDatabaseBundle:UzytkownikProjekt up WITH p.id = up.projekt
-                WHERE up.uzytkownik = :uzytkownik_id
-                AND p.skasowane = true
-        ")->setParameter(':uzytkownik_id', $uzytkownik->getId())->getResult();
-        
-                $projektSubQuery = $m->createQuery("                    
-            SELECT pp.id
-                        FROM DataDatabaseBundle:UzytkownikProjekt up
-                        JOIN DataDatabaseBundle:Projekt pp WITH pp.id = up.projekt
-                        WHERE up.uzytkownik = :uzytkownik_id
-        ")
-                ->setParameter(':uzytkownik_id', $uzytkownik->getId())
-                ->getResult();
-        $projekt = $m->createQuery("
-            SELECT p
-                FROM DataDatabaseBundle:Projekt p
-                WHERE p.id NOT IN (
-                    :array
-                )
-               AND p.skasowane = true
-        ")
-                ->setParameter(':status_zamkniety', Projekt::STATUS_ZAMKNIETY)
-                ->setParameter(':array', $projektSubQuery)
-                ->getResult();
-        return $this->render('AppFrontendBundle:Projects:index.html.twig', array(
-                    'myProjects' => $collMyProjekt,
-                    'restProjects' => $projekt,
-                    'UzytkownikProjekt' => new UzytkownikProjekt(),
-                    'Projekt' => new Projekt()
-        ));
         return $this->render('AppFrontendBundle:Projects:skasowane.html.twig', array(
                     'myProjects' => $collMyProjekt,
-                    'restProjects' => $projekt,
+                    'restProjects' => $restProject,
                     'UzytkownikProjekt' => new UzytkownikProjekt(),
                     'Projekt' => new Projekt()
         ));
